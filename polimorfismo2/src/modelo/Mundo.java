351aeb491a8c23fd82647ba5e45517341da1e16b
aulab213 <aulab213@UBSCOB213-20.salas.edu.co>
2019-09-16 10:40:35 -0500

    ??

--- polimorfismo2/src/modelo/Mundo.java
+++ polimorfismo2/src/modelo/Mundo.java
@@ -9,12 +9,15 @@ public class Mundo {
 	}
 	
 	public String hacerPolimorfismo() {		
+		
 		String linea = "";
 		personas[0] = new Persona();
 		personas[1] = new Alumno();
 		personas[2] = new Profesor();
+		
 		for (int i=0 ; i<3; i++)
 		linea += personas[i]+" \n";
 		return linea;
 		}
+	
 }
