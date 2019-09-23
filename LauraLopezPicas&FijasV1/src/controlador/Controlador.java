e01b63906d7ac96e12aabe1a53f13da03b343860
Laura López <Laura Lpez@LAPTOP-1LGA081A>
2019-09-23 10:57:24 -0500

    act

--- LauraLopezPicas&FijasV1/src/controlador/Controlador.java
+++ LauraLopezPicas&FijasV1/src/controlador/Controlador.java
@@ -3,16 +3,19 @@ package controlador;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
+
 import modelo.Mundo;
 import vistaGUI.InterfazGUI;
 import vistaGUI.PanelEntrada;
 
 public class Controlador implements ActionListener{
 	
-	@SuppressWarnings("unused")
 	private Mundo mundo;
 	
 	private InterfazGUI gui;
+	
+	int contador=2;
 	
 	public Controlador() {
 		
@@ -25,80 +28,46 @@ public class Controlador implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent evento) {
 		
-		gui.getPanelEntrada();
+		gui.getPanelEntrada();		
 		
-		int contador=2;
+		int[] numerosParaAdivinar = mundo.getJuego().numerosPorAdivinar();
 			
 		if (evento.getActionCommand().equals(PanelEntrada.PROBAR)) {
 			
-			
+			if (contador<12) {
 				
-			int a = Integer.parseInt(gui.getPanelEntrada().getTxtNumerosParaProbar1().getText());
-			int b = Integer.parseInt(gui.getPanelEntrada().getTxtNumerosParaProbar2().getText());
-			int c = Integer.parseInt(gui.getPanelEntrada().getTxtNumerosParaProbar3().getText());
-			int d = Integer.parseInt(gui.getPanelEntrada().getTxtNumerosParaProbar4().getText());
-			
-			int[] nums = new int[4];
+				int numerosParaProbar[] = new int[4];
 				
-			nums[0]=a;
-			nums[1]=b;
-			nums[2]=c;
-			nums[3]=d;
+				for (int i = 0; i < numerosParaProbar.length; i++) {
+					numerosParaProbar[i] = Integer.parseInt(gui.getPanelEntrada().getTxtNumerosParaProbar0().getText().substring(i, i+1));
+				}
+						
+				int picas = mundo.getJuego().darNumPicas(numerosParaAdivinar, numerosParaProbar);
+				int fijas = mundo.getJuego().darNumFijas(numerosParaAdivinar, numerosParaProbar);
+						
+				gui.getPanelResultados().getTxtPicas().setText(picas + "");
+				gui.getPanelResultados().getTxtFijas().setText(fijas + "");
+				
+				if (fijas==4) {
+					JOptionPane.showMessageDialog(null, "¡Felidades, lo adivinaste!");
+				}
+				
+				gui.getPanelEntrada().getTxtNumerosParaProbar0().setText("");				
 					
-			int picas = mundo.getJuego().darNumPicas(nums);
-			int fijas = mundo.getJuego().darNumFijas(nums);
-					
-			gui.getPanelResultados().getTxtPicas().setText(picas + "");
-			gui.getPanelResultados().getTxtFijas().setText(fijas + "");
-			
-			gui.getPanelEntrada().getTxtNumerosParaProbar1().setText("");
-			gui.getPanelEntrada().getTxtNumerosParaProbar2().setText("");
-			gui.getPanelEntrada().getTxtNumerosParaProbar3().setText("");
-			gui.getPanelEntrada().getTxtNumerosParaProbar4().setText("");
+				gui.getPanelEntrada().getLabIntento1().setText("Intento "+contador);
 				
-			
+				contador++;	
 				
-			gui.getPanelEntrada().getLabIntento1().setText("Intento "+contador);
-			
-			contador++;	
-			
+			} else {
+				
+				String nums = numerosParaAdivinar[0] + " " + numerosParaAdivinar[1] + " " + numerosParaAdivinar[2] + " " + numerosParaAdivinar[3] ; 
+
+
+				JOptionPane.showMessageDialog(null, "Excediste el número de intentos" + "Respuesta: "+ nums);
 			}
 		
 		
 		}		
-		
-		
-			
-		
-		/*if (evento.getActionCommand().equals(PanelEntrada.PROBAR)) {
-			
-			int a = Integer.parseInt(gui.getPanelEntrada().getTxtNumerosParaProbar1().getText());
-			int b = Integer.parseInt(gui.getPanelEntrada().getTxtNumerosParaProbar2().getText());
-			int c = Integer.parseInt(gui.getPanelEntrada().getTxtNumerosParaProbar3().getText());
-			int d = Integer.parseInt(gui.getPanelEntrada().getTxtNumerosParaProbar4().getText());
-			
-			int[] nums = new int[4];
-			
-			nums[0]=a;
-			nums[1]=b;
-			nums[2]=c;
-			nums[3]=d;
-			
-			int picas = mundo.getJuego().darNumPicas(nums);
-			int fijas = mundo.getJuego().darNumFijas(nums);
-			
-			gui.getPanelResultados().getTxtPicas().setText(picas + "");
-			gui.getPanelResultados().getTxtFijas().setText(fijas + "");
-			
-		}*/
-		
-	
-
-	/*public void actionPerformed(ActionEvent evento) {
-		gui.getPanelEntrada();
-		if (evento.getActionCommand().equals(PanelEntrada.PROBAR)) {
-			gui.getPanelResultados().getTxtMonto().setText(gui.getPanelEntrada().getTxtNumero().getText()+" Procesado");
-		}
-	}*/
+	}
 
 }
--- LauraLopezPicas&FijasV1/src/modelo/Juego.java
+++ LauraLopezPicas&FijasV1/src/modelo/Juego.java
@@ -2,20 +2,33 @@ package modelo;
 
 public class Juego {
 	
-	private int[] numerosPorAdivinar;
+	//private int[] numerosPorAdivinar;
 	
 	public Juego() {
 
-		numerosPorAdivinar = new int[4];
-		numerosPorAdivinar[0]=3;
+		//numerosPorAdivinar = new int[4];
+		/*numerosPorAdivinar[0]=3;
 		numerosPorAdivinar[1]=2;
 		numerosPorAdivinar[2]=7;
-		numerosPorAdivinar[3]=8;
+		numerosPorAdivinar[3]=8;*/
 		
 	}
 	
 
-	public int darNumFijas(int[] numerosParaProbar) {
+	public int[] numerosPorAdivinar() {
+		
+		int[] numerosPorAdivinar = new int[4];
+		
+		for (int i = 0; i < numerosPorAdivinar.length; i++) {
+			
+			
+			numerosPorAdivinar[i] = 1 + (int)(Math.random() * ((9 - 1) + 1));
+		}
+		
+		return numerosPorAdivinar;
+	}
+	
+	public int darNumFijas(int[] numerosPorAdivinar, int[] numerosParaProbar) {
 		
 		int contadorFijas = 0;
 		
@@ -27,7 +40,7 @@ public class Juego {
 		return contadorFijas;		
 	}
 	
-	public int darNumPicas(int[] numerosParaProbar) {
+	public int darNumPicas(int[] numerosPorAdivinar, int[] numerosParaProbar) {
 		
 		int contadorPicas = 0;		
 		
